d3642c256dd61428220a2e3930515845c4823d82
akashsp12 <akashsp12@outlook.com>
2024-01-18 16:21:53 +0530

    worked on google sign method

--- midddleWare/userRoute.js
+++ midddleWare/userRoute.js
@@ -4,6 +4,7 @@ const userController = require('../controller/user')
 
 router.post('/create-account', userController.postuser)
 router.post('/login', userController.login)
+router.post('/google-login-method', userController.googleLogin)
 
 
 
