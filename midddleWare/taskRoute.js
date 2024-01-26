d8599941d90108fb637c98c715a370f053a0e4bd
Akashsp12 <akashsp12@outlook.com>
2024-01-26 21:23:33 +0530

    worked on remove the todo

--- midddleWare/taskRoute.js
+++ midddleWare/taskRoute.js
@@ -7,6 +7,8 @@ router.post('/add-new-task', jwtController.auth, taskController.addTask)
 router.post('/get-task-from-db', jwtController.auth, taskController.getTaskFromDb)
 router.get('/edit-task/:id', taskController.editTask)
 router.put('/update-task-with-id', taskController.updateTask)
+router.get('/update-status-on-todo/:id', taskController.updateTaskStatus)
+router.delete('/remove-on-todo/:id', taskController.removeTaskFormDB)
 
 
 module.exports = router
